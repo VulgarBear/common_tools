563f68ac52f23bafe569e172db76dc37ed7a5f6d
VulgarBear <lucas@shisify.com>
2022-07-14 08:44:57 -0400

    feat(PhantomBot): :hammer: Created backup script for phantom bot.
    Created backup script for Phantom Bot. Takes user inputed parameter during running of the script and then creates tar.gz file and moves to auto_backups folder.

---
+++ scripts/phantomBackup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Config
+# Using '-e' ensures the script will exit if it encounters an error.
+set -e
+set -o pipefail
+
+# Variables
+BOT=$1
+TWITCH_PATH="${HOME}/twitch"
+BOT_PATH="${TWITCH_PATH}/${BOT}"
+DATE=`date +%m-%d-%Y`
+
+# Sources
+source $(dirname $0)/utils/color_codes.sh
+source $(dirname $0)/utils/logging_utils.sh
+
+traperr() {
+	echo "ERROR: ${BASH_SOURCE[1]} at about ${BASH_LINENO[0]}"
+}
+
+set -o errtrace
+trap traperr ERR
+
+# SCRIPT START
+c_hilight "Beginning Phantom Bot Backup"
+sleep 1
+
+# Check and Create backups folder if missing
+c_info "Checking for backups folder..."
+
+if [ -d "${TWITCH_PATH}/auto_backups" ]; then
+	c_success "auto_backups folder exists"
+fi
+if [[ ! -d "${TWITCH_PATH}/auto_backups" ]]; then
+	mkdir "${TWITCH_PATH}/auto_backups"
+	c_success "Created auto_backups folder."
+fi
+
+# Begin Backup Steps
+c_info "Backing up $BOT..."
+sleep 1
+
+tar --exclude="${BOT_PATH}/lib" --exclude="${BOT_PATH}/web" -czf "${BOT_PATH}_$DATE.tar.gz" "${BOT_PATH}"
+
+c_info "Created ${BOT_PATH}/${BOT}_$DATE.tar.gz"
+sleep 1
+
+# Clean Up
+c_info "Cleaning up..."
+sleep 1
+
+mv "${BOT_PATH}/${BOT}_$DATE.tar.gz" "${TWITCH_PATH}/auto_backups"
+
+c_hilight "Backup Completed"
+sleep 1
---
+++ utils/logging_utils.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+source $(dirname $0)/utils/color_codes.sh
+
+set -e
+
+echo ''
+
+c_info() {
+  printf "\n ⭐${BGrey} %s${NC}\n" "$@"
+  sleep 1
+}
+
+c_question() {
+  printf "\n ❔${BCyan} %s${NC}\n" "$@"
+  sleep 1
+}
+
+c_success() {
+  printf "\n ✔️ ${BGreen} %s${NC}\n" "$@"
+  sleep 1
+}
+
+c_error() {
+  printf "\n ❌ ${Red} %s${NC}\n" "$@"
+  sleep 1
+}
+
+c_warning() {
+  printf "\n ⚠️ ${BYellow} %s${NC}\n" "$@"
+  sleep 1
+}
+
+c_install() {
+  printf "\n 🚧 ${Grey} %s${NC}\n" "$@"
+}
+
+c_hilight() {
+  printf "${LightPurple} %s${NC}" "$@"
+}
